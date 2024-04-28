ab49d53d872fcbd7def623e30f08036a87dbdb07
Giuseppe Spina <giuseppespina@192.168.88.28>
2024-04-28 18:35:33 +0200

    chages-bfore-i18n

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,15 +1,68 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  LoginPageState createState() {
+    return LoginPageState();
+  }
+}
+
+class LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Page'),
-      ),
-      body: Center(
-        child: Text('Welcome to My Page!'),
-      ),
+        appBar: AppBar(
+          title: Text('Login Page'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+                child: Form(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  key: formKey,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Name',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Name is required';
+                            }
+                            return null;
+                          }
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty || !value.contains('@')) {
+                            return 'Invalid email address';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (formKey.currentState?.validate() == true) {
+                            formKey.currentState!.save();
+                          }
+                        },
+                        child: Text('Submit'),
+                      ),
+                    ],
+                  ),
+                )
+            )
+        )
     );
   }
 }
