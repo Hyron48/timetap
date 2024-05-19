cfad1c9616c4d31979a3c2c34982f716502ae2eb
Giuseppe Spina <giuseppespina@192.168.88.28>
2024-05-19 19:09:25 +0200

    mpl-home-page-for-nfc

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,15 +1,37 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:timetap/bloc/auth/auth_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  Future<void> triggerNfcAndGeoLocalization() async {
+    print('trigger');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('OK'),
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          GestureDetector(
+            onTap: triggerNfcAndGeoLocalization,
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/svg/add_clock_in.svg',
+                width: (MediaQuery.of(context).size.width / 2.4),
+                height: (MediaQuery.of(context).size.height / 2.4),
+              ),
+            ),
+          ),
+        ],
       ),
     );
     return Column(
