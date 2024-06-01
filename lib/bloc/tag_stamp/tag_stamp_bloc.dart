c1e2280f31b9360a137fc5478909e3e0bef112af
Giuseppe Spina <giuseppespina@192.168.88.13>
2024-06-01 19:01:28 +0200

    nfc-impl-completed

---
+++ lib/bloc/tag_stamp/tag_stamp_bloc.dart
@@ -0,0 +1,4 @@
+import 'package:equatable/equatable.dart';
+
+part 'home_events.dart';
+part 'home_state.dart';
---
+++ lib/bloc/tag_stamp/tag_stamp_state.dart
@@ -0,0 +1,5 @@
+part of 'home_bloc.dart';
+
+class BaseHomeState extends Equatable {
+  final bool clocke
+}
