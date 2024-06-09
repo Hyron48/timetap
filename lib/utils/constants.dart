8b3f8321748f0e38c51d505d7d8a32861ee33ba3
Giuseppe Spina <giuseppespina@192.168.88.13>
2024-06-10 00:09:37 +0200

    restiling-login-registraton-page

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color bluePrimary = Color.fromARGB(100, 0, 77, 168);
+const Color bluePrimary = Color.fromARGB(255, 0, 77, 168);
 const Color white = Colors.white;
 const Color offWhite = Color(0xFFFBFBFB);
 const Color grey = Color(0xFFF7F7F7);
@@ -10,5 +10,40 @@ const Color darkGrey = Color(0xFF6E6E6E);
 const Color colorStatusSand = Color.fromARGB(255, 126, 118, 110);
 const Color graphite = Color(0xFF3D3D3D);
 const Color black = Colors.black;
-final Color lightGold = const Color(0xFFDFCFB6).withAlpha(70);
 const Color colorStatusRed = Color.fromARGB(255, 195, 0, 20);
+
+const double defaultSidePadding = 13;
+
+const double fontSizeTitle = 24.0;
+const double fontSizeSubtitle = 20.0;
+const double fontSizeText = 16.0;
+
+const FontWeight lightFontWeight = FontWeight.w300;
+const FontWeight mediumFontWeight = FontWeight.w500;
+const FontWeight boldFontWeight = FontWeight.w700;
+const FontWeight boldestFontWeight = FontWeight.w900;
+
+
+const OutlineInputBorder textFieldBorder = OutlineInputBorder(
+  borderSide: BorderSide(
+      color: lightGrey,
+      width: 1.0
+  ),
+  borderRadius: BorderRadius.all(
+      Radius.circular(15.0)
+  ),
+);
+
+OutlineInputBorder textFieldBorderError = textFieldBorder.copyWith(
+  borderSide: const BorderSide(color: Colors.red),
+);
+
+OutlineInputBorder multilineTextFieldBorder = textFieldBorder.copyWith(
+  borderRadius: const BorderRadius.all(
+      Radius.circular(15.0)
+  ),
+);
+
+OutlineInputBorder multilineErrorTextFieldBorder = multilineTextFieldBorder.copyWith(
+  borderSide: const BorderSide(color: Colors.red),
+);
