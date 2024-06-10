84e089f5af7954b6730e44ad5de3ea0cc76b9abb
Giuseppe Spina <giuseppespina@192.168.88.13>
2024-06-10 20:55:45 +0200

    drawer

--- lib/ui/shared/drawer.dart
+++ lib/ui/shared/drawer.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:timetap/bloc/auth/auth_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:timetap/utils/routes.dart';
 
+import '../../utils/constants.dart';
+import 'generic_info_row.dart';
 import 'language_dropdown.dart';
 
 class NavigatorDrawer extends StatelessWidget {
@@ -20,55 +23,89 @@ class NavigatorDrawer extends StatelessWidget {
         Navigator.of(context).pushReplacementNamed(Routes.initialRoute);
       }
     },
-    child: Drawer(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.all(24),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Text(context.read<AuthBloc>().getUserEmail()),
-                      LanguageDropdown(),
-                    ],
-                  ),
-                  ListTile(
-                    leading: const Icon(
-                      Icons.home_outlined,
-                      color: Colors.black,
+    child: Container(
+      width: MediaQuery.of(context).size.width / 1.5, // Restringe il drawer
+      child: Drawer(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.fromLTRB(24.0, 75.0, 0, 50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/svg/logo-only-text.svg',
+                          width: MediaQuery.of(context).size.width / 35,
+                          height: MediaQuery.of(context).size.height / 35,
+                        ),
+                        Spacer(),
+                        LanguageDropdown(),
+                      ],
                     ),
-                    title: Text(
-                      AppLocalizations.of(context)?.menuHome ?? 'Not Found',
+                    const SizedBox(height: 24.0),
+                    Text(
+                      AppLocalizations.of(context)?.email ?? 'Not Found',
+                      style: TextStyle(color: darkGrey, fontSize: 13),
                     ),
-                    onTap: () => Navigator.of(context)
-                        .pushReplacementNamed(Routes.initialRoute),
-                  ),
-                  ListTile(
-                    leading: const Icon(
-                      Icons.list,
-                      color: Colors.black,
+                    Text(
+                      context.read<AuthBloc>().getUserEmail(),
+                      style: TextStyle(color: black, fontSize: 16),
                     ),
-                    title: Text(
-                      AppLocalizations.of(context)?.menuHistory ?? 'Not Found',
+                    const SizedBox(height: 24.0),
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: const Icon(
+                        Icons.home_outlined,
+                        color: bluePrimary,
+                      ),
+                      title: Text(
+                        AppLocalizations.of(context)?.menuHome ?? 'Not Found',
+                        style: TextStyle(fontWeight: lightFontWeight),
+                      ),
+                      onTap: () => Navigator.of(context)
+                          .pushReplacementNamed(Routes.initialRoute),
                     ),
-                    onTap: () => Navigator.of(context)
-                        .pushReplacementNamed(Routes.tagHistoryRoute),
-                  ),
-                ],
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      leading: const Icon(
+                        Icons.list,
+                        color: bluePrimary,
+                      ),
+                      title: Text(
+                        AppLocalizations.of(context)?.menuHistory ?? 'Not Found',
+                        style: TextStyle(fontWeight: lightFontWeight),
+                      ),
+                      onTap: () => Navigator.of(context)
+                          .pushReplacementNamed(Routes.tagHistoryRoute),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 0, 0, 50.0),
-            child: ElevatedButton(
-              onPressed: () => logout(context),
-              child: Text(AppLocalizations.of(context)?.menuLogout ?? 'Not Found'),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 50.0),
+              child:  OutlinedButton(
+                onPressed: () => logout(context),
+                style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: bluePrimary),
+                    backgroundColor: white,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 12.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    )),
+                child: Text(
+                  AppLocalizations.of(context)?.menuLogout ??
+                      'Not Found',
+                  style: TextStyle(color: bluePrimary),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
